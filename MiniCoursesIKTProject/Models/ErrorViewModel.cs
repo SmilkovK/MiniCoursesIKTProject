a8e3b4cdf7f310e64d19088c74cd89ed21514145
SmilkovK <kire.smilkov@students.finki.ukim.mk>
2025-03-20 21:10:26 +0100

    Add project files.

---
+++ MiniCoursesIKTProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniCoursesIKTProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
