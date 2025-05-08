c1d701b79f3da734530e3f5dc9fcc506d8a9f074
milaanastasova <anastasova.mila@gmail.com>
2025-05-08 18:23:12 +0200

    removed grades view for students

--- MiniCoursesIKTProject/Controllers/UsersController.cs
+++ MiniCoursesIKTProject/Controllers/UsersController.cs
@@ -79,17 +79,6 @@ namespace MiniCoursesIKTProject.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Grades(string id)
-        {
-            var user = await _userService.GetByIdAsync(id);
-            if (user == null)
-                return NotFound();
-
-            //TODO - Add service method for listing subjects for student
-            return View(user);
-        }
-
-        [HttpGet]
         public async Task<IActionResult> Edit(string id)
         {
             var user = await _userService.GetUserWithRolesByIdAsync(id);
